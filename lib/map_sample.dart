50b9da9df2ae3e9ae5990d2770381cc936d46c1d
MudasirHussain72 <mudasirhussain41302@@gmail.com>
2022-10-06 23:29:19 +0500

    convert longLat into address & viceversa

--- lib/map_sample.dart
+++ lib/map_sample.dart
@@ -4,44 +4,64 @@ import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapSample extends StatefulWidget {
+  const MapSample({super.key});
+
   @override
-  State<MapSample> createState() => MapSampleState();
+  State<MapSample> createState() => _MapSampleState();
 }
 
-class MapSampleState extends State<MapSample> {
+class _MapSampleState extends State<MapSample> {
+  // google map cntroller
   Completer<GoogleMapController> _controller = Completer();
-
-  static final CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
-    zoom: 14.4746,
+  // current camera position
+  static final CameraPosition _kGooglePlex = const CameraPosition(
+    target: LatLng(24.8607, 67.0011),
+    zoom: 14,
   );
+  List<Marker> _marker = [];
 
-  static final CameraPosition _kLake = CameraPosition(
-      bearing: 192.8334901395799,
-      target: LatLng(37.43296265331129, -122.08832357078792),
-      tilt: 59.440717697143555,
-      zoom: 19.151926040649414);
+  List<Marker> _list = const [
+    Marker(
+        markerId: MarkerId("1"),
+        position: LatLng(24.8607, 67.0011),
+        infoWindow: InfoWindow(title: "karachi")),
+    Marker(
+        markerId: MarkerId("2"),
+        position: LatLng(25.3960, 68.3578),
+        infoWindow: InfoWindow(title: "hyderabad")),
+  ];
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _marker.addAll(_list);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      body: GoogleMap(
-        mapType: MapType.hybrid,
-        initialCameraPosition: _kGooglePlex,
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
-        },
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: _goToTheLake,
-        label: Text('To the lake!'),
-        icon: Icon(Icons.directions_boat),
-      ),
-    );
-  }
-
-  Future<void> _goToTheLake() async {
-    final GoogleMapController controller = await _controller.future;
-    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
+    return Scaffold(
+        body: SafeArea(
+          child: GoogleMap(
+            onMapCreated: (GoogleMapController controller) {
+              _controller.complete(controller);
+            },
+            markers: Set.of(_marker),
+            compassEnabled: true,
+            myLocationEnabled: true,
+            myLocationButtonEnabled: true,
+            initialCameraPosition: _kGooglePlex,
+            mapType: MapType.normal,
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () async {
+            GoogleMapController controller = await _controller.future;
+            controller.animateCamera(CameraUpdate.newCameraPosition(
+              CameraPosition(target: LatLng(25.3960, 68.3578), zoom: 14),
+            ));
+            setState(() {});
+          },
+          child: Icon(Icons.abc),
+        ));
   }
 }
